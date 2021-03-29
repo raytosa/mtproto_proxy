a8d5b02bebe5dd7150648f038d9c05acc883b587
raytosa <rayto_sa@163.com>
2021-03-29 09:47:14 +0800

    tk 03

--- src/mtp_down_conn.erl
+++ src/mtp_down_conn.erl
@@ -351,7 +351,6 @@ down_send(Packet, #state{sock = Sock, codec = Codec, dc_id = DcId} = St) ->
       end, #{labels => [DcId]}),
     {ok,St#state{codec = Codec1}}.
 
-
 up_send(Packet, ConnId, #state{upstreams_rev = UpsRev} = St) ->
      %%io_lib:format("mtp_down_conn      up_send ~n"),
     case maps:find(ConnId, UpsRev) of
