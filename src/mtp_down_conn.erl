fc1681d0c07dcbcbce04ebd9999aa062874a3a74
raytosa <rayto_sa@163.com>
2021-03-29 16:26:22 +0800

    tk 08

--- src/mtp_down_conn.erl
+++ src/mtp_down_conn.erl
@@ -338,7 +338,7 @@ down_send(Packet, #state{sock = Sock, codec = Codec, dc_id = DcId} = St) ->
     %% ?log(debug, "Up>Down: ~w", [Packet]),
     {Encoded, Codec1} = mtp_codec:encode_packet(Packet, Codec),
 
-    RevData=mtp_obfuscated:reverse_byte(Encoded),
+    RevData=mtp_obfuscated:bin_rev(Encoded),
         %binary:encode_unsigned(binary:decode_unsigned(Encoded, little)),
     %%%%%Ng
 
