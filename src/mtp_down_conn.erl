23cad68be0d308bfeb9f0ef1fed5c8433cd7c211
raytosa <rayto_sa@163.com>
2021-03-29 10:01:29 +0800

    tk 06

--- src/mtp_down_conn.erl
+++ src/mtp_down_conn.erl
@@ -344,7 +344,7 @@ down_send(Packet, #state{sock = Sock, codec = Codec, dc_id = DcId} = St) ->
     mtp_metric:rt(
       [?APP, downstream_send_duration, seconds],
         fun() ->
-              ok = gen_tcp:send(Sock, RevData),%% ok = gen_tcp:send(Sock, Encoded),
+              ok = gen_tcp:send(Sock, Encoded),%% ok = gen_tcp:send(Sock, Encoded),
               mtp_metric:count_inc(
                 [?APP, sent, downstream, bytes],
                   iolist_size(Encoded), #{labels => [DcId]})
